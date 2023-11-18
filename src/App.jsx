2626cec5e839b21eae21ce86522ff641736d274d
pyungjae9492 <ppj050@snu.ac.kr>
2023-11-18 13:19:50 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 import { TextInput } from './components/TextInput'
 
 function App() {
-  const [messageList, setMessageList] = useState(["스무고개 게임을 시작할게요, 제가 생각하는 단어를 맞춰주세요!"])
+  const [messageList, setMessageList] = useState([
+    {
+      role: "assistant",
+      content: "스무고개 게임을 시작할게요, 제가 생각하는 단어를 맞춰주세요!"
+    }
+  ])
 
   const addMessage = (message) => {
-    setMessageList([...messageList, message])
+    setMessageList([...messageList, { role: "user", content: message }])
   }
   
   return (
@@ -14,9 +19,14 @@ function App() {
       <div className='max-w-[500px] h-full w-full flex flex-col justify-center items-center bg-slate-200 py-10 px-5 gap-4'>
         <div className='w-full h-full bg-white rounded-xl p-10 flex flex-col gap-2'>
           {messageList.map((message) => {
+
+            const isBot = message.role === 'assistant'
+
             return (
-              <div className='w-full h-10 rounded-xl bg-slate-500 text-white flex items-center px-4'>
-                {message}
+              <div className={`flex w-full ${isBot ? "justify-start" : "justify-end"}`}>
+                <div className={`max-w-[80%] rounded-xl text-white flex items-center px-4 py-2 ${isBot ? "bg-slate-500" : "bg-slate-300"}`}>
+                  {message.content}
+                </div>
               </div>
             )
           })}
